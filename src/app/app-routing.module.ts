b91c84de27636225c5f4924ddd065173c78a780d
Adrian Juszczak <adrian.juszczak@datawalk.com>
2020-06-27 09:11:12 +0200

    Foundation
    Signed-off-by: Adrian Juszczak <adrian.juszczak@datawalk.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CounterComponent } from './counter/counter.component';
+import { TodosComponent } from './todos/todos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'counter',
+  },
+  {
+    path: 'counter',
+    component: CounterComponent,
+  },
+  {
+    path: 'todos',
+    component: TodosComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
