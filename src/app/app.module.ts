f08ebbff127aa1c9db20fa8f5e3ab4e35621bd65
Adrian Juszczak <adrian.juszczak@datawalk.com>
2020-06-27 10:16:04 +0200

    DevTools
    Signed-off-by: Adrian Juszczak <adrian.juszczak@datawalk.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { CounterComponent } from './counter/counter.component';
 import { TodosComponent } from './todos/todos.component';
 import { StoreModule } from '@ngrx/store';
 import { counterReducer } from './counter/counter.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, CounterComponent, TodosComponent],
@@ -18,6 +20,7 @@ import { counterReducer } from './counter/counter.reducer';
       },
       {},
     ),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
   ],
   providers: [],
   bootstrap: [AppComponent],
